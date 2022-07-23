74fac76099d79b83d6487d4eccdc3e4ac5e2c63c
Ahmed Atef <81183868+zerocool-coder@users.noreply.github.com>
2022-07-23 09:16:38 +0200

    Life in weeks calculator
    calculates the remaining days,months ,weeks  in human life from 90 years old!

---
+++ Life in weeks calculator.py
@@ -0,0 +1,9 @@
+age = input("What is your current age?")
+age = 90 - int(age)
+days = age * 365
+weeks = age * 52
+months = age * 12
+print(f"you have {days} days,{weeks} weeks,and {months} months left")
+
+
+
